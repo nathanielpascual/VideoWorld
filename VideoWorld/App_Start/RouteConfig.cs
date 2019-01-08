5694fa14c17ec9f5fec4e852f8d9655078c756c7
nathanielpascual <nath_pascual@yahoo.com>
2019-01-08 17:29:45 +0800

    added bootstrap and movies model, controller and viewmodel

--- VideoWorld/App_Start/RouteConfig.cs
+++ VideoWorld/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace VideoWorld
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+			routes.MapMvcAttributeRoutes();
+
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
---
+++ VideoWorld/Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoWorld.Models;
+using VideoWorld.ViewModel;
+namespace VideoWorld.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movies
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Random()
+		{
+			Movie movie = new Movie() { Id = 1, Name = "Equalizer" };
+
+			var customers = new List<Customer>
+			{
+				new Customer{ Id=1,Name= "Customer1"},
+				new Customer{ Id=1,Name= "Customer2"},
+			};
+
+			var viewModel = new RandomMovieViewModel
+			{
+				Movie = movie,
+				Customers = customers
+			};
+
+
+			return View(viewModel);
+		}
+
+		[Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)}")]
+		public ActionResult ByReleaseDate(int year, int month) {
+
+			return Content(year + "/" + month);
+		}
+	}
+}
