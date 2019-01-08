9da97e0cbe46dbda624090670eb52c4f3e4cf436
nathanielpascual <nath_pascual@yahoo.com>
2019-01-08 14:53:50 +0800

    Add project files.

---
+++ VideoWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoWorld.Startup))]
+namespace VideoWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
